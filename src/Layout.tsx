e218c8d10c9dd681d16fee0ad99223407d209d92
Amogh <amoghvnadkarni001@gmail.com>
2025-04-29 11:18:02 +0100

    Update Sidebar Submenus

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,8 +1,11 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
+import Topbar from "./components/topbar"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
+    <>
+    <Topbar/>
     <SidebarProvider>
       <AppSidebar />
       <main>
@@ -10,5 +13,6 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         {children}
       </main>
     </SidebarProvider>
+    </>
   )
 }
