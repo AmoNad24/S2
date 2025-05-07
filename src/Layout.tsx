740fa363ba9d770aa28000c3e80e1c3896c021cf
Amogh <amoghvnadkarni001@gmail.com>
2025-05-07 04:12:57 +0100

    Fix Sidebar

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -4,6 +4,8 @@ import {Topbar} from "./components/topbar"
 import OverviewPage from "./components/Overview"
 import CompliancePage from "./components/Compliance"
 import IAMPage from "./components/Iam"
+import {Outlet} from 'react-router-dom';
+
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
@@ -11,9 +13,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
             <Topbar/>
             <SidebarProvider>
                 <Sidebar />
-                <main className="flex-1 overflow-y-auto p-6 lg:pl-[275px] transition-all duration-300"> {/* Added lg:pl-[275px] and transition */}
-                    {/* <OverviewPage/> */}
-                    <IAMPage/>
+                <main className="flex-1 overflow-y-auto p-8  transition-all duration-300">
+                    <Outlet/>
+                    {/* <IAMPage/> */}
                 </main>
             </SidebarProvider>
         </>
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,24 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 // import App from './App.tsx'
 import Layout from './Layout.tsx'
+import { createBrowserRouter,createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import OverviewPage from './components/Overview.tsx'
+import IAMPage from './components/Iam.tsx'
+import CompliancePage from './components/Compliance.tsx'
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={ <Layout children={[]}/>}>
+      <Route path='' element={<OverviewPage/>} />
+      <Route path='/IAM' element={<IAMPage/>} />
+      <Route path='/Compliance' element={<CompliancePage/>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     {/* <App /> */}
-    <Layout children={[]}/>
+    {/* <Layout children={[]}/> */}
+    <RouterProvider router={router}/>
   </StrictMode>
 )
